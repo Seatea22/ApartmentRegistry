50e441da37bb31fcc100be2be0fe542524cff1bb
Caleb Talbott <calebtalbott22@gmail.com>
2022-06-24 14:45:00 -0400

    Data Entry Additions
    The GUI:
    + A ChoiceBox for the months
    + A submit button for the two text fields to submit room id and tenant name.
    + A list view for each month's rooms
    
    The Main Controller:
    + Implements Initializable to run an initial method to initialize various things.
    + A method to refresh the list view.
    + A method to submit values entered into the text fields.

--- src/main/java/com/apartmentregistry/MainController.java
+++ src/main/java/com/apartmentregistry/MainController.java
@@ -3,23 +3,39 @@ package com.apartmentregistry;
 import com.apartmentregistry.classes.Room;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable {
     @FXML
     public MenuItem openFileButton;
+    public ChoiceBox<String> monthBar;
+    public ListView<String> listView;
+    public Button submitButton;
+    public TextField roomIdField;
+    public TextField tenantField;
 
-    private HashMap<String, Room> monthData;
+    private HashMap<String, ArrayList<Room>> monthDirectory = new HashMap<>();
+    private String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
     private File currentFile;
     FileHandler fh = new FileHandler();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        for (String month : months)
+            monthDirectory.put(month, new ArrayList<>());
+        monthBar.getItems().addAll(months);
+        monthBar.setOnAction(this::monthMenuClick);
+    }
 
     public void openFilePressed(ActionEvent actionEvent) throws IOException {
         currentFile = fh.loadFile();
@@ -39,6 +55,27 @@ public class MainController {
                     "November\n\n" +
                     "December");
             fileWriter.close();
+
+
         }
+    }
+
+    public void monthMenuClick(ActionEvent event) {
+        refreshList();
+    }
+
+    public void refreshList() {
+        listView.getItems().clear();
+        ArrayList<Room> rooms = monthDirectory.get(monthBar.getValue());
+        for (Room room : rooms) {
+            listView.getItems().add(room.getRoomId() + " : " + room.getAttendant());
+        }
+    }
+
+    public void submitPressed(ActionEvent actionEvent) {
+        if (!roomIdField.getText().isEmpty() && !tenantField.getText().isEmpty()) {
+            monthDirectory.get(monthBar.getValue()).add(new Room(Integer.parseInt(roomIdField.getText()), tenantField.getText()));
+        }
+        refreshList();
     }
 }
